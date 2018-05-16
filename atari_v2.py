0a246b603539b5f5c4910e23528e100918647f7a
Mattias Walla <mattias.walla@gmail.com>
2018-05-16 17:39:31 +0200

    preetrain

--- atari_v2.py
+++ atari_v2.py
@@ -14,17 +14,17 @@ from threading import Thread
 
 K.set_image_dim_ordering('th')
 
-class trainThread (Thread):
-    def __init__(self, threadID, name):
-        Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-
-
-    def set_args(self, args):
-        self.args = args
-    def run(self):
-        fit_batch(self.args[0], self.args[1], self.args[2], self.args[3], self.args[4], self.args[5], self.args[6], )
+#class trainThread (Thread):
+#    def __init__(self, threadID, name):
+#        Thread.__init__(self)
+#        self.threadID = threadID
+#        self.name = name
+#
+#
+#    def set_args(self, args):
+#        self.args = args
+#    def run(self):
+#        fit_batch(self.args[0], self.args[1], self.args[2], self.args[3], self.args[4], self.args[5], self.args[6], )
 
 
 class RingBuf:
@@ -58,6 +58,35 @@ class RingBuf:
     def __iter__(self):
         for i in range(len(self)):
             yield self[i]
+    def reward_batch(self):
+        batch = []
+        for i in range(self.__len__()):
+            if(self.data[i][2] > 0):
+                i
+                for x in range (i-REWARD_FRAME_SIZE,i):
+                    batch.insert(0, x%self.__len__() )
+            if(len(batch) > BATCH_SIZE * REWARD_FRAME_SIZE):
+                break
+
+                
+        size = len(batch)
+        start_states = np.zeros((size, numFrames, 104, 80))
+        actions = np.zeros((size, NUM_ACTIONS))
+        rewards = np.zeros((size))
+        next_states = np.zeros((size, numFrames, 104, 80))
+        is_terminal = []
+        index = 0
+        
+        for x in batch :
+            for i in range (numFrames) :
+                start_states[index][numFrames-1-i] = self.data[(x-i-1)%self.__len__()][1]
+                next_states[index][numFrames-1-i] = self.data[(x-i)%self.__len__()][1]
+            actions[index][self.data[x][0]] = 1
+            rewards[index] = self.data[x][2]
+            is_terminal.append(self.data[x][3])
+            index += 1
+        return (start_states, actions, rewards, next_states, np.array(is_terminal))
+        
     def sample_batch(self, size):
        # if(self.__len__() == self.size):
         #print(self.__len__())
@@ -69,10 +98,10 @@ class RingBuf:
             #subdata = self.data[0:self.end]
             #print(subdata)
             #sample = np.random.choice(subdata, size, replace=False)
-        start_states = np.zeros((size, numFrames, 105, 80))
-        actions = np.zeros((size, 4))
+        start_states = np.zeros((size, numFrames, 104, 80))
+        actions = np.zeros((size, NUM_ACTIONS))
         rewards = np.zeros((size))
-        next_states = np.zeros((size, numFrames, 105, 80))
+        next_states = np.zeros((size, numFrames, 104, 80))
         is_terminal = []
         index = 0
         #print(randIndex)
@@ -96,14 +125,14 @@ def downsample(img):
 
 
 def preprocess(img):
-    return to_grayscale(downsample(img))
+    return to_grayscale(downsample(img[0:208,:,:]))
 
 
 def transform_reward(reward):
     return np.sign(reward)
 
 
-def fit_batch(model, gamma, start_states, actions, rewards, next_states, is_terminal):
+def fit_batch(model, gamma, start_states, actions, rewards, next_states, is_terminal, epochs_n):
     """Do one deep Q learning iteration.
 
     Params:
@@ -128,22 +157,23 @@ def fit_batch(model, gamma, start_states, actions, rewards, next_states, is_term
     # the targets by the actions.
     model.fit(
         [start_states, actions], actions * Q_values[:, None],
-        epochs=1, batch_size=len(start_states), verbose=0
+        epochs=epochs_n, batch_size=len(start_states), verbose=0
     )
-def pre_train(model, frames):
+
+    
+def pre_train(model, frames, epochs_n):
     # First, predict the Q values of the next states. Note how we are passing ones as the mask.
 
     # Fit the keras model. Note how we are passing the actions as the mask and multiplying
     # the targets by the actions.
-    #print(frames)
+    # print(frames.shape)
     model.fit(
         frames, frames,
-        epochs=1, batch_size=len(frames), verbose=0
+        epochs=epochs_n, batch_size=len(frames), verbose=0
     )
 
 def pretraining_model():
-    # We assume a theano backend here, so the "channels" are first.
-    ATARI_SHAPE = (numFrames, 105, 80)
+    
 
     # With the functional API we need to define the inputs.
     frames_input = keras.layers.Input(ATARI_SHAPE, name='frames')
@@ -152,24 +182,40 @@ def pretraining_model():
     normalized = keras.layers.Lambda(lambda x: x / 255.0, output_shape=ATARI_SHAPE)(frames_input)
 
     # "The first hidden layer convolves 16 8×8 filters with stride 4 with the input image and applies a rectifier nonlinearity."
-    conv_1 = keras.layers.Convolution2D(
+    conv_1 = keras.layers.Conv2D(
         16, (8, 8), activation='relu', padding='same'
     )(normalized)
-    
+
+    pooling_1 = keras.layers.MaxPooling2D(pool_size=(4, 4))(conv_1)
+    # "The second hidden layer convolves 32 4×4 filters with stride 2, again followed by a rectifier nonlinearity."
+    conv_2 = keras.layers.Conv2D(
+        32, (4, 4), activation='relu', padding='same'
+    )(pooling_1)    
+
+
 
     # Autoencoding
-    x = keras.layers.Convolution2D(16, (8, 8), activation='sigmoid', padding='same')(conv_1)
-    decoded = keras.layers.Convolution2D(numFrames, (8, 8), activation='sigmoid', padding='same')(x)
-    model = keras.models.Model(frames_input, decoded)
+    decoded_1 = keras.layers.Conv2D(
+        32, (4, 4), activation='relu', padding='same'
+    )(conv_2)
+
+    up_1 = keras.layers.UpSampling2D((4,4))(decoded_1)
+    
+    decoded_2 = keras.layers.Conv2D(
+        numFrames, (8, 8), activation='sigmoid', padding='same'
+    )(up_1)
+    
+    unnormalized = keras.layers.Lambda(lambda x: x*255.0, output_shape = ATARI_SHAPE)(decoded_2)
+    model = keras.models.Model(frames_input, unnormalized)
     optimizer = keras.optimizers.RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
     model.compile(optimizer, loss='mse')
 
     return model
 
-def atari_model(n_actions):
-    # We assume a theano backend here, so the "channels" are first.
-    ATARI_SHAPE = (numFrames, 105, 80)
+def atari_model( n_actions):
 
+    
+    
     # With the functional API we need to define the inputs.
     frames_input = keras.layers.Input(ATARI_SHAPE, name='frames')
     actions_input = keras.layers.Input((n_actions,), name='mask')
@@ -178,22 +224,19 @@ def atari_model(n_actions):
     normalized = keras.layers.Lambda(lambda x: x / 255.0, output_shape=ATARI_SHAPE)(frames_input)
 
     # "The first hidden layer convolves 16 8×8 filters with stride 4 with the input image and applies a rectifier nonlinearity."
-    conv_1 = keras.layers.convolutional.Convolution2D(
-        16, 8, 8, subsample=(4, 4), activation='relu'
-    )(normalized)
-
-    #Autoencoding
-    x = keras.layers.convolutional.Convolution2D(16, (8, 8), activation='relu', padding='same')(conv_1)
-    decoded = keras.layers.convolutional.Convolution2D(numFrames, (8, 8), activation='sigmoid', padding='same')(x)
-
-
-    #end of autoencoding
+    conv_1 = keras.layers.Conv2D(
+        16, (8, 8),  activation='relu'
+    )(normalized)    
+    
+    pooling_1 = keras.layers.MaxPooling2D(pool_size=(4, 4))(conv_1)
     # "The second hidden layer convolves 32 4×4 filters with stride 2, again followed by a rectifier nonlinearity."
-    conv_2 = keras.layers.convolutional.Convolution2D(
-        32, 4, 4, subsample=(2, 2), activation='relu'
-    )(conv_1)
+    conv_2 = keras.layers.Conv2D(
+        32, (4, 4), activation='relu'
+    )(pooling_1)
+    pooling_2 = keras.layers.MaxPooling2D(pool_size = (2, 2))(conv_2)
+
     # Flattening the second convolutional layer.
-    conv_flattened = keras.layers.Flatten()(conv_2)
+    conv_flattened = keras.layers.Flatten()(pooling_2)
 
     # "The final hidden layer is fully-connected and consists of 256 rectifier units."
     hidden = keras.layers.Dense(256, activation='relu')(conv_flattened)
@@ -203,20 +246,21 @@ def atari_model(n_actions):
     filtered_output = keras.layers.multiply([output, actions_input])
 
     model = keras.models.Model(input=[frames_input, actions_input], output=filtered_output)
-    optimizer = optimizer = keras.optimizers.RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
+    optimizer = keras.optimizers.RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
     model.compile(optimizer, loss='mse')
 
     return model
 
 
-def get_epsilon_for_iteration(iteration):
-    num_iterations = 4000
-    if iteration < 4000:
-        return 1 - 0.9/(iteration+1)
+def get_epsilon_for_iteration(iteration):    
+    if iteration < LIMIT_1 :
+        return 1
+    elif iteration < LIMIT_2:
+        return 1 +(VALUE - 1)*(iteration -LIMIT_1)/(LIMIT_2 - LIMIT_1)
     else:
-        return 0.1
+        return VALUE
 
-def q_iteration(env, model, state, iteration, memory):
+def q_iteration(env, model, state, iteration, memory, model_pre):
     # Choose epsilon based on the iteration
     epsilon = get_epsilon_for_iteration(iteration)
 
@@ -239,34 +283,36 @@ def q_iteration(env, model, state, iteration, memory):
     new_frame, reward, is_done, _ = env.step(action)
     new_frame = preprocess(new_frame)
     memory.append((action, new_frame, reward, is_done))
-
+    if(reward > 0):
+        print(reward)
     #state = state.copy()
     for i in range(numFrames-1):
         state[0][i-1] = state[0][i+1]
     state[0][numFrames - 1] = new_frame
-    #state[0][0] = state[0][1]
-    #state[0][1] = state[0][2]
-    #state[0][2] = state[0][3]
-    #state[0][3] = new_frame
 
 
     #Sample and fit
 
     if (iteration > BATCH_SIZE):
         batch = memory.sample_batch(BATCH_SIZE)
-        if(iteration > 3000):
+        if(iteration > PRETRAINING_TIMES):
 
             #   print(iteration)
-            #fit_batch(model, 0.99, batch[0], batch[1], batch[2], batch[3], batch[4])
-
-            thread.set_args([model, 0.99, batch[0], batch[1], batch[2], batch[3], batch[4]])
-            thread.run()
+            fit_batch(model, 0.99, batch[0], batch[1], batch[2], batch[3], batch[4], EPOCH)
+            
+            #thread.set_args([model, 0.99, batch[0], batch[1], batch[2], batch[3], batch[4]])
+            #thread.run()
+        elif(iteration == PRETRAINING_TIMES) :
+            #importera vikter
+            #print (model_pre.summary())
+            #print (model_pre.get_weights())
+            print(model_pre.evaluate(batch[0],batch[0], BATCH_SIZE))
+            #print (model.summary())
+            #print (model.get_weights())
+            a=1
         else:
-            pre_train(model_pre, batch[0])
-    if (iteration % 1000 == 0):
-        print("saved:")
-        print(iteration)
-        model.save("model_weights_2018_05_09_V3.HDF5")
+            a=1
+            pre_train(model_pre, batch[0], PRE_EPOCH)
     return (state, is_done);
 
 def choose_best_action(model, state):
@@ -276,11 +322,28 @@ def choose_best_action(model, state):
     return output.argmax()
 
 
-buffer_size = 200000
-thread = trainThread(1, "Thread-1")
+buffer_size = 500000
+#thread = trainThread(1, "Thread-1")
 numFrames = 2
-model_pre = pretraining_model()
-BATCH_SIZE = 32
+NUM_ACTIONS = 4
+BATCH_SIZE = 64
+PRE_EPOCH = 8
+EPOCH = BATCH_SIZE//32
+REWARD_BATCH_EPOCH = 16
+PRETRAINING_TIMES = 10000
+REWARD_FRAME_SIZE = 32
+
+# We assume a theano backend here, so the "channels" are first.
+ATARI_SHAPE = (numFrames, 104, 80)
+
+#epsilon
+LIMIT_1 = PRETRAINING_TIMES #epsilon = 1 untill this number
+LIMIT_2 = 500000 #after this number epsilon = value
+VALUE = 0.1
+
+
+
+
 def main():
     config = tf.ConfigProto(intra_op_parallelism_threads=4, \
                             inter_op_parallelism_threads=4, allow_soft_placement=True, \
@@ -288,7 +351,9 @@ def main():
     session = tf.Session(config=config)
     K.set_session(session)
 
-    model = atari_model(4)
+    model = atari_model(NUM_ACTIONS)  
+    model_pre = pretraining_model()
+    print (model_pre.summary())
     #model = keras.models.load_model("model_weights_2018_05_09_V3.HDF5")
     # Create a breakout environment
     env = gym.make('BreakoutDeterministic-v4')
@@ -307,26 +372,47 @@ def main():
     action = env.action_space.sample()
     frame, reward, is_done, _ = env.step(action)
 
-    frames = np.zeros((1, numFrames,105,80))
+    frames = np.zeros((1, numFrames,104,80))
     frame = preprocess(frame)
     frames[0][0] = frame
-    actions = np.ones((1,4))
+    actions = np.ones((1,NUM_ACTIONS))
     #print(actions)
     state = frames
+    benchmark = 0
     while True:
 
-
+        
 
         env.render()
 
-        (state, is_done) = q_iteration(env, model, state, i, memoryBuffer)
+        (state, is_done) = q_iteration(env, model, state, i, memoryBuffer, model_pre)
         i += 1
 
         # Perform a random action, returns the new frame, reward and whether the game is over
         #frame, reward, is_done, _ = env.step(env.action_space.sample())
+        if i == PRETRAINING_TIMES :
+            weights_1 = model_pre.get_layer("conv2d_3").get_weights()
+            weights_2 = model_pre.get_layer("conv2d_4").get_weights()            
+            model.get_layer("conv2d_1").set_weights(weights_1)
+            model.get_layer("conv2d_2").set_weights(weights_2)
+            batch = memoryBuffer.reward_batch()
+            if( len(batch[0]) >0 ):                
+                fit_batch(model, 0.99, batch[0], batch[1], batch[2], batch[3], batch[4], REWARD_FRAME_SIZE )
+            
+        if i >= PRETRAINING_TIMES and i % 5000 ==0 :
+            print("saved:")
+            print(iteration)
+            print(benchmark)
+            model.save("model_weights_2018_05_16_PRETRAIN.HDF5")
+            benchmark = 0
+            batch = memoryBuffer.reward_batch()
+            if( len(batch[0]) >0 and i < LIMIT_2): 
+                fit_batch(model, 0.99, batch[0], batch[1], batch[2], batch[3], batch[4], REWARD_FRAME_SIZE )
+                
+            
         if (is_done):
             env.reset()
-            print("reset")
+            benchmark += 1
             is_done = False
 
 
